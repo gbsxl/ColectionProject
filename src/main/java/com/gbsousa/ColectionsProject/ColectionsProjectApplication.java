5ffe9691a9e818b58677ebfeb181c0c69ebac3a7
gbsxl <gasousaoli@gmail.com>
2024-11-13 08:47:54 -0300

    Project Created

---
+++ src/main/java/com/gbsousa/ColectionsProject/ColectionsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.gbsousa.ColectionsProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColectionsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColectionsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gbsousa/ColectionsProject/ColectionsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gbsousa.ColectionsProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColectionsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
